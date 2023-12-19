90e1c4a43a7b2b501a0ef6043c6aae968d628034
JeonInWoo <hodolee@illunex.com>
2023-12-19 16:25:20 +0900

    feat : factsheet project init

---
+++ src/test/java/com/illunex/factsheetadminapi/FactsheetAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.illunex.factsheetadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FactsheetAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
